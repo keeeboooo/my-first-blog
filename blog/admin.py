7b3c8ce599e0c8aaad73c9a2be1ddac86037d1ea
keeeboooo <keisuke_onepiece@icloud.com>
2021-06-14 19:17:53 +0900

    delete comment drift

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment, #Rogin
 
 admin.site.register(Post)
-
+admin.site.register(Comment)
+#admin.site.register(Rogin)
 # Register your models here.
